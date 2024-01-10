f6dccb4e72fe839ee0e3de276f14678cb34bfa67
Ayowale <haydexwisdom@gmail.com>
2024-01-10 17:07:27 +0100

    Task 4 done

--- 0x04-TypeScript/task_1/main.ts
+++ 0x04-TypeScript/task_1/main.ts
@@ -18,3 +18,27 @@ export interface printTeacherFunction{
 export function printTeacher(firstName: string, lastName: string): string {
   return `${firstName.substring(0, 1)}. ${lastName}`;
 }
+
+export interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInt;
+}
+
+export interface StudentClassInt {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements StudentClassInt{
+  private firstName: string;
+  private lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework() {
+    return 'Currently working';
+  }
+  displayName() {
+    return this.firstName;
+  }
+}
